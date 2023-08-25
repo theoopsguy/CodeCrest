4e82727950dbb2c9ed0b79ce953256f1eeddd5b6
theoopsguy <theoopsguy01@gmail.com>
2023-08-26 01:22:41 +0530

    Add loading state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import { Home } from './container';
 import { auth } from './config/firebase.config';
+import Spinner from './components/Spinner';
 
 const App = () => {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
@@ -12,18 +14,28 @@ const App = () => {
       } else {
         navigate('/home/auth', { replace: true });
       }
+
+      setIsLoading(false);
     });
 
     // clean up the listener event
     return () => unsubscribe();
   }, []);
   return (
-    <div className="w-screen h-screen flex items-start justify-start overflow-hidden">
-      <Routes>
-        <Route path="/home/*" element={<Home />} />
-        <Route path="*" element={<Navigate to="/home" />} />
-      </Routes>
-    </div>
+    <>
+      {isLoading ? (
+        <div className="w-screen h-screen flex items-center justify-center overflow-hidden">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="w-screen h-screen flex items-start justify-start overflow-hidden">
+          <Routes>
+            <Route path="/home/*" element={<Home />} />
+            <Route path="*" element={<Navigate to="/home" />} />
+          </Routes>
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ src/components/Spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ThreeCircles } from 'react-loader-spinner';
+
+const Spinner = () => {
+  return (
+    <ThreeCircles
+      height="100"
+      width="100"
+      color="#4fa94d"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel="three-circles-rotating"
+      outerCircleColor=""
+      innerCircleColor=""
+      middleCircleColor=""
+    />
+  );
+};
+
+export default Spinner;
